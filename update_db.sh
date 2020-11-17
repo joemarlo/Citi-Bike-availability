9aa8cfea1db6e4c8432c5adb6166ea1b390e4032
joemarlo <joe.marlo.09@gmail.com>
2020-11-16 19:41:29 -0500

    added shell script to handle cron job

---
+++ update_db.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH="$PYTHONPATH:/home/joemarlo/Dropbox/Data/Projects/Citi-Bike-availability"
+python3 /home/joemarlo/Dropbox/Data/Projects/Citi-Bike-availability/Modeling/make_predictions.py
